e04724275d5c6b1be1e991ccba92cf47d0c3a11a
arunavade99 <arunavade99@gmail.com>
2024-04-04 15:01:01 +0530

    flask impleamented and running successfully

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,33 +1,44 @@
-import sys, pandas as pd
+import sys
+import os
+import pandas as pd
 from projects.src.exception import CustomException
 from projects.src.utils import load_object
 
+
 class PredictPipeline:
-    def __int__(self):
+    def __init__(self):
         pass
 
     def predict(self, features):
         try:
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path = 'artifacts\preprocessor.pkl'
+            # C:\Users\Arunava\Desktop\projects\src\components\artifacts
+            model_path='C:/Users/Arunava/Desktop/projects/src/components/artifacts/model.pkl'
+            preprocessor_path='C:/Users/Arunava/Desktop/projects/src/components/artifacts/preprocessor.pkl'
+
+            # model_path = os.path.join("artifacts", "model.pkl")
+            # preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+            print("Before Loading")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
             return preds
+
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
+
 
 class CustomData:
-    def __int__(self,
-                gender: str,
-                race_ethnicity: str,
-                parental_level_of_education: str,
-                lunch: str,
-                test_preparation_course: str,
-                reading_score: float,
-                writing_score: float
-                ):
+    def __init__(self,
+                 gender: str,
+                 race_ethnicity: str,
+                 parental_level_of_education,
+                 lunch: str,
+                 test_preparation_course: str,
+                 reading_score: int,
+                 writing_score: int):
+
         self.gender = gender
 
         self.race_ethnicity = race_ethnicity
@@ -51,7 +62,7 @@ class CustomData:
                 "lunch": [self.lunch],
                 "test_preparation_course": [self.test_preparation_course],
                 "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score]
+                "writing_score": [self.writing_score],
             }
 
             return pd.DataFrame(custom_data_input_dict)
